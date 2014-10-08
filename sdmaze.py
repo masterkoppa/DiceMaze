5bc25645ad9434898ab9aaea4e6605413e6731da
Andres Ruiz <andresruiz2010@gmail.com>
2014-10-08 17:47:18 -0400

    Change heuristics name

--- sdmaze.py
+++ sdmaze.py
@@ -18,7 +18,7 @@ def euclidian_distance(maze):
 
 	return math.sqrt(math.pow(c_row - c_col, 2) + math.pow(g_row - g_col, 2))
 
-def multi_goal_distance(maze):
+def brooklyn_distance(maze):
 	c_row, c_col = maze.current
 	g_row, g_col = maze.goal
 
@@ -73,7 +73,6 @@ def aStar(graph, initialMaze, heuristic):
 				came_from[n] = current
 
 	# No solution
-	print("No solution")
 	print("Nodes visited: " + str(nodes_visited))
 	print("Nodes generated: " + str(nodes_generated))
 
@@ -83,6 +82,23 @@ def buildPath(parent_graph, goalNode):
 	else:
 		return buildPath(parent_graph, parent_graph[goalNode]) + [goalNode]
 
+def getDirection(m_from, m_to):
+	from_row, from_col = m_from.current
+	to_row, to_col = m_to.current
+
+	diff_row = from_row - to_row
+	diff_col = from_col - to_col
+
+	if diff_row == 0 and diff_col == 1:
+		return "West"
+	elif diff_row == 0 and diff_col == -1:
+		return "East"
+	elif diff_row == 1 and diff_col == 0:
+		return "North"
+	elif diff_row == -1 and diff_col == 0:
+		return "South"
+	else:
+		return "Invalid"
 
 def main():
 	args = sys.argv
@@ -107,9 +123,9 @@ def main():
 
 	#print("Dummy Heuristic")
 	#results = aStar(graph, initialMaze, dummy_heuristic)
-	#print("-------------------------------------------")
-	print("Multi-Goal")
-	results = aStar(graph, initialMaze, multi_goal_distance)
+	print("-------------------------------------------")
+	print("Brooklyn Distance")
+	results = aStar(graph, initialMaze, brooklyn_distance)
 	print("-------------------------------------------")
 
 	#Nuke the graph
@@ -126,10 +142,24 @@ def main():
 	results = aStar(graph, initialMaze, euclidian_distance)
 	print("-------------------------------------------\n\n")
 
-	#for i in results:
-	#	print(i)
+	if results != None:
+		print("Starting Position")
+		print("-----------------")
+		print(results[0])
 
-	#print(len(results))
+		for i in range(1, len(results)):
+			print("-----------------")
+			print("Move: " + str(i))
+			m_from = results[i-1]
+			m_to = results[i]
+			print("Direction: " + getDirection(m_from, m_to))
+			print("-----------------")
+			print(results[i])
+
+
+		print("Number of move to goal: " + str(len(results)))
+	else:
+		print("No Solution")
 
 
 if __name__ == '__main__':
